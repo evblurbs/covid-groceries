3d911c8c4561965e49f71a278389640fc7922d39
Evan Johnson <evblurbs@Evans-Mac-Pro.local>
2020-03-31 22:33:10 -0700

    Add some basic styles via grommett and formik for forms.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,58 @@
-import React from 'react';
-import { Grommet } from 'grommet';
+import React from "react";
+import { Box, Grommet, Heading, Button } from "grommet";
+import { Notification } from "grommet-icons";
 
 const theme = {
   global: {
+    colors: {},
     font: {
-      family: 'Roboto',
-      size: '18px',
-      height: '20px',
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
     },
   },
 };
 
+const AppBar = (props: any) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
+
 function App() {
   return (
-    <Grommet theme={theme}>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          Hello
+          <Heading level="3" margin="none">
+            Covid Groceries
+          </Heading>
+          <Button icon={<Notification />} onClick={() => {}} />
+        </AppBar>
+        <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+          <Box flex align="center" justify="center">
+            app body
+          </Box>
+          <Box
+            width="medium"
+            background="light-2"
+            elevation="small"
+            align="center"
+            justify="center"
+          >
+            sidebar
+          </Box>
+        </Box>
+      </Box>
     </Grommet>
   );
 }
