c6ebce34f089bd6fe2f778463cb2bee78d41da02
Brian Cama <briancama@gmail.com>
2020-03-31 23:56:25 -0700

    WIP Add radio buttons for bundles

--- src/Bundles.tsx
+++ src/Bundles.tsx
@@ -1,6 +1,25 @@
-import React from "react";
-import { Box } from "grommet";
+import React, { useState } from "react";
+import { Box, RadioButtonGroup } from "grommet";
 
-const Bundles = (props: any) => <Box pad="large">Bundles</Box>;
+const Bundles = () => {
+  const [value, setValue] = useState();
+  return (
+    <Box pad="large" align="center" justify="between">
+      Bundles
+      <RadioButtonGroup
+        name="bundles"
+        direction="row"
+        gap="medium"
+        pad="large"
+        options={[
+          { label: "Essentials", value: "essentials" },
+          { label: "Frozen Pack", value: "frozen" }
+        ]}
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+    </Box>
+  );
+};
 
 export default Bundles;
