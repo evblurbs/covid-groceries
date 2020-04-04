3349faf674e96d1fa1486816dd83ca1943085aad
Brian Cama <briancama@gmail.com>
2020-04-04 13:47:23 -0700

    Add responsive route container and bundles

--- src/recipient/Bundles.tsx
+++ src/recipient/Bundles.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { Box, RadioButtonGroup, Text, Heading } from "grommet";
+import {
+  Box,
+  RadioButtonGroup,
+  Text,
+  Heading,
+  ResponsiveContext
+} from "grommet";
 import { Bundle, Cafeteria } from "grommet-icons";
 import Navigate from "../form/Navigate";
 import Header from "../components/Header";
@@ -8,73 +14,106 @@ import { StepCallback } from "../interfaces";
 
 const Bundles = ({ next }: StepCallback) => {
   const [bundle, setBundle] = useState();
+  const size = React.useContext(ResponsiveContext);
+  const [disabled, setDisabled] = useState(true);
+  const bundlesData = [
+    {
+      label: "Essentials",
+      value: "essentials",
+      icon: Bundle,
+      description:
+        "From toothpaste to toilet paper, help provide those in need with essential items for daily life."
+    },
+    {
+      label: "Frozen",
+      value: "frozen",
+      icon: Cafeteria,
+      description:
+        "Frozen items can be stored for long periods of time and turned into delicious meals quickly."
+    }
+  ];
   const onSubmit = () =>
     next({
       screenId: screenIds.BUNDLES,
-      inputs: { bundle },
+      inputs: { bundle }
     });
   return (
     <Box
       width="large"
       height={{
-        min: "unset",
+        min: "unset"
       }}
     >
-      <Header>Bundles</Header>
-      <RadioButtonGroup
-        name="bundles"
-        direction="row"
-        gap="medium"
-        pad="large"
-        height={{
-          min: "unset",
-        }}
-        options={[
-          {
-            label: "Essentials",
-            value: "essentials",
-          },
-          {
-            label: "Frozen",
-            value: "frozen",
-          },
-        ]}
-        value={bundle}
-        onChange={(event) => setBundle(event.target.value)}
-      >
-        {(
-          option: any,
-          { checked, hover }: { checked: Boolean; hover: Boolean }
-        ) => {
-          const Icon = option.value === "essentials" ? Bundle : Cafeteria;
-          let background;
-          if (checked) background = "brand";
-          else if (hover) background = "light-4";
-          else background = "light-2";
-          return (
-            <Box
-              pad="large"
-              align="center"
-              background={background}
-              round
-              width={{ min: "small" }}
-              gap="small"
-            >
-              <Icon size="large" />
-              <Heading
-                level={4}
-                margin={{
-                  bottom: "small",
-                }}
+      <fieldset style={{ border: "none", padding: 0 }}>
+        <legend>
+          <Header>Bundles</Header>
+        </legend>
+
+        <RadioButtonGroup
+          name="bundle"
+          justify="center"
+          align="center"
+          direction={size === "small" ? "column" : "row"}
+          gap="medium"
+          height={{
+            min: "unset"
+          }}
+          options={bundlesData.map((bundle, index) => {
+            return {
+              label: bundle.label,
+              value: bundle.value,
+              name: "bundle",
+              index: index
+            };
+          })}
+          value={bundle}
+          wrap={true}
+          onChange={event => {
+            setBundle(event.target.value);
+            setDisabled(false);
+          }}
+        >
+          {(
+            option: any,
+            { checked, hover }: { checked: Boolean; hover: Boolean }
+          ) => {
+            let currentBundle = bundlesData[option.index];
+            const Icon = currentBundle.icon;
+            let background;
+            if (checked) {
+              background = "brand";
+            } else if (hover) background = "light-4";
+            else background = "light-2";
+            return (
+              <Box
+                pad="large"
+                align="center"
+                background={background}
+                round
+                gap="none"
+                width={size === "small" ? "100%" : "320px"}
               >
-                {option.label}
-              </Heading>
-              <Text>Description of bundle will go here</Text>
-            </Box>
-          );
-        }}
-      </RadioButtonGroup>
-      <Navigate onClick={onSubmit} backPath={PATH_DELIVERY} />
+                <Icon size="large" />
+                <Heading
+                  level={4}
+                  margin={{
+                    bottom: "small"
+                  }}
+                >
+                  {option.label}
+                </Heading>
+                <Text>{currentBundle.description}</Text>
+              </Box>
+            );
+          }}
+        </RadioButtonGroup>
+      </fieldset>
+
+      <Navigate
+        disabled={disabled}
+        onClick={onSubmit}
+        backPath={PATH_DELIVERY}
+      />
     </Box>
   );
 };
