621849bd7511d18e14a20809e5afea2aefb49520
Evan Johnson <evblurbs@Evans-Mac-Pro.local>
2020-04-09 01:24:45 -0700

    Enable analytics.

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,6 +1,7 @@
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/database";
+import "firebase/analytics";
 import { FIREBASE_API_KEY } from "./constants";
 
 const firebaseConfig = {
@@ -15,6 +16,8 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+firebase.analytics();
 
 export const db = firebase.firestore();
 export const realTimeDb = firebase.database();
