24a5d5f7b04c06f79614b47bfc50d95271427679
Evan Johnson <evblurbs@Evans-Mac-Pro.local>
2020-04-09 00:54:43 -0700

    Make carousel responsive.

--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading, Grid, Paragraph } from "grommet";
+import { Box, Heading, Grid, Paragraph, ResponsiveContext } from "grommet";
 
 interface Card {
   bgColor: string;
@@ -8,26 +8,48 @@ interface Card {
   Icon: any;
 }
 
-const CarouselCard = ({ bgColor, header, description, Icon }: Card) => (
-  <Box
-    pad={{
-      top: "none",
-      left: "xlarge",
-      right: "xlarge",
-      bottom: "xlarge",
-    }}
-    background={bgColor}
-  >
-    <Heading level={3} size="large" color="dark-2">
-      {header}
-    </Heading>
-    <Grid columns={["auto", "flex"]} gap="xlarge">
-      <Icon size="xlarge" />
-      <Paragraph size="xlarge" margin="none" color="dark-2">
-        {description}
-      </Paragraph>
-    </Grid>
-  </Box>
-);
+const CarouselCard = ({ bgColor, header, description, Icon }: Card) => {
+  const size = React.useContext(ResponsiveContext);
+  const isSmall = size === "small";
+  return (
+    <Box
+      pad={{
+        top: "none",
+        left: "xlarge",
+        right: "xlarge",
+        bottom: "xlarge",
+      }}
+      background={bgColor}
+      align={isSmall ? "center" : "start"}
+    >
+      <Heading
+        level={isSmall ? 2 : 3}
+        size={isSmall ? "medium" : "large"}
+        color="dark-2"
+        textAlign={isSmall ? "center" : "start"}
+      >
+        {header}
+      </Heading>
+      <Grid
+        columns={isSmall ? ["fill"] : ["auto", "flex"]}
+        gap={isSmall ? "medium" : "xlarge"}
+        align={isSmall ? "center" : "start"}
+      >
+        <Icon
+          size={isSmall ? "large" : "xlarge"}
+          style={isSmall ? { margin: "0 auto" } : undefined}
+        />
+        <Paragraph
+          size={isSmall ? "medium" : "xlarge"}
+          margin="none"
+          color="dark-2"
+          textAlign={isSmall ? "center" : "start"}
+        >
+          {description}
+        </Paragraph>
+      </Grid>
+    </Box>
+  );
+};
 
 export default CarouselCard;
